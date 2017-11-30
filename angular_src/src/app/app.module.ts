6d66a7e90a75882749e997f8594f08edcfb0f59c
Jorge Muñoz <mrjleo1989@gmail.com>
2017-11-30 11:34:40 -0500

    setting editor

--- angular_src/src/app/app.module.ts
+++ angular_src/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './components/portada/portada.component';
+
+import { EditorModule, ButtonModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular_src/src/app/components/portada/portada.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portada',
+  templateUrl: './portada.component.html',
+  styleUrls: ['./portada.component.css']
+})
+export class PortadaComponent implements OnInit {
+
+  text: string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
